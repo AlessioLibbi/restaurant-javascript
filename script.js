8d8f8e00738d9813bec8fc2ac708bf315de0b3d9
Alessio Libbi <alex.libbi@gmail.com>
2023-11-20 15:30:37 +0100

    Add: local host

--- script.js
+++ script.js
@@ -17,11 +17,28 @@ const menu = [
   // Altri piatti ....
 ];
 
-var selectType = document.querySelector('#tipo-piatto');
+// Select tipo piatto
+
+
+// Ordine LocalStorage
+var order = JSON.parse(localStorage.getItem('savedOrder')) ?? []
+
+var total = 0
+
+
+
+// Totale ordine
+var priceCounter = []
 
 function generaSelectTipo() {
 
   var tipi = []
+  var selectType = document.querySelector('#tipo-piatto');
+  tipi.push('Seleizona categoria')
+  optDef = document.createElement("option")
+  optDef.innerHTML = 'Seleziona categoria'
+  optDef.value = ""
+  selectType.appendChild(optDef)
   menu.forEach(element => {
     if (!tipi.includes(element.tipo)) {
       tipi.push(element.tipo)
@@ -34,7 +51,6 @@ function generaSelectTipo() {
 }
 
 function showPiatti(tipo) {
-
   var plateList = document.querySelector('#piatti')
   plateList.innerHTML = ""
   var plateToShow = menu.filter(plate => plate.tipo === tipo)
@@ -45,42 +61,45 @@ function showPiatti(tipo) {
     plateList.appendChild(plateDiv)
     plateDiv.addEventListener("click", () => addPiatto(plate))
   })
-
-
-
 }
-let order = []
+
 function addPiatto(plate) {
-  var orderList = document.querySelector('.order-list')
   alert("Aggiunto " + plate.nome + " all'ordine");
   order.push(plate)
-  var orderDiv = document.createElement("div")
-  orderDiv.classList.add("ordine-singolo")
-  orderDiv.innerHTML = plate.nome
-  orderList.appendChild(orderDiv)
-
-  
-  getPrice(plate)
+  localStorage.setItem('savedOrder', JSON.stringify(order));
+  showOrder();
 }
-var priceCounter = []
-function getPrice (plate) {
-  
 
+function showOrder(){
+  
+  var orderList = document.querySelector('.order-space')
+  orderList.innerHTML = ""
+  order.forEach(plate => {
+    var orderDiv = document.createElement("div")
+    orderDiv.classList.add("ordine-singolo")
+    orderDiv.innerHTML = plate.nome
+    orderList.appendChild(orderDiv)
+  })
+  getPrice()
+
+}
+
+
+function getPrice() {
+
+  total = 0
   var priceList = document.querySelector('.price-list')
   var dollarCount = document.createElement("p")
   dollarCount.classList.add('price-counter')
 
-  priceCounter.push(plate.prezzo)
-
-  var total = 0;
-  priceCounter.forEach((price)=> {
-    total +=price
-  } )
+  order.forEach((plate) => {
+    total += plate.prezzo
+  })
+  console.log(total);
+  localStorage.setItem('setPrice', total );
   
   //total= priceCounter.reduce(function(a, b) { return a + b; }, 0);
-  priceList.innerHTML = `Il tuo conto e ${total}`
-   
-  console.log(priceCounter)
+  priceList.innerHTML = `Il tuo conto e ${total}€`
   priceList.appendChild(dollarCount)
 }
 
